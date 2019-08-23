dc6bc62323450565ceb593c6d8682bcf039766b5
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-23 12:03:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,16 +20,18 @@ def find_max_value(array)
   max
 end
 
+=begin
 def find_min_value(array)
    array.sort[0]
 end
+=end 
 
 def find_min_value(array)
-  min = 0
   array.length.times do |count|
    if count ==  0 then
       min = array[count]
     elsif
+      min = array[0]
       array[count] < min
       min = array[count]
    end
